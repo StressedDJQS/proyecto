afc7c728217ba5ffb8b1a73ee2150dee4ca02f31
StressedDJQS <dquispesaa@unsa.edu.pe>
2024-05-12 18:19:38 -0500

    Codigo final de InversorMatriz

---
+++ Laboratorio2/InversorMatriz.java
@@ -0,0 +1,19 @@
+public class InversorMatriz {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al inversor de matrices");
+        int [] array = {1,2,3,4,5,6};
+        int []arrayInv;
+        arrayInv=invertirArray(array);
+        for(int i=0;i<arrayInv.length;i++){
+            System.out.print(arrayInv[i]+", ");
+        }
+    }
+    public static int [] invertirArray(int[] a){
+        int arrayInvertido[] = new int[a.length];
+        for(int i=0; i<a.length;i++){
+            arrayInvertido[i]=a[a.length-i-1];
+        }
+        return arrayInvertido;
+    }
+}
